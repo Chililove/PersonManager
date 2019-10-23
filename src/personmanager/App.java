0d1ac08bcbe685dc1439d24e9a7d4d5cbd5162a8
Chililove <mega_lulu_kryster@hotmail.com>
2019-10-23 14:22:56 +0200

    Next

--- src/personmanager/App.java
+++ src/personmanager/App.java
@@ -5,6 +5,8 @@
  */
 package personmanager;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mega_
@@ -16,7 +18,21 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-     Person person = new Person("Jeppe");
+        Person person = new Person("Jeppe");
         System.out.println(person.getName());
+        Person person1 = new Person("Peter");
+        System.out.println(person1.getName());
+        Person person2 = new Person("Tobias");
+        System.out.println(person2.getName());
+        ArrayList<String> personer;
+        personer = new ArrayList<String>();
+        personer.add(person.getName());
+        personer.add(person1.getName());
+        personer.add(person2.getName());
+        System.out.println (personer.toString());
+        
+    }
+    public static void Person(String[] args) {
+        
     }
 }
