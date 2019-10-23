b92fb4d5e07b7c3ed6a0fafd78a5331ff426ed25
Chililove <mega_lulu_kryster@hotmail.com>
2019-10-23 13:15:39 +0200

    New added

---
+++ src/personmanager/Person.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personmanager;
+
+/**
+ *
+ * @author mega_
+ */
+public class Person {
+ private String name;
+ 
+ public Person(String n)
+ {
+     name=n;
+     
+ }
+    public String getName(){
+        return name;
+    }
+}
